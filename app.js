1c3a8a45b90959de9b980419e6c4c058d6a2e838
José Cardoso <caardoso4@outlook.com>
2023-08-30 17:53:18 -0300

    modified

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const submitInput = document.querySelector(".submit-input");
 const myList = document.querySelector(".items-list");
 
 submitInput.addEventListener("click", ()=>{
-    let li = document.createElement("li");
-    li.textContent = qntInput.value;
-    myList.appendChild(li);
+    if (qntInput.value === ''){
+        alert("Empty field");
+    }else{
+        let li = document.createElement("li");
+        li.textContent = qntInput.value;
+        myList.appendChild(li);
+    }
 });
