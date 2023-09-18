b89ab0b75ae576f21100774fd48e2b756db4e054
José Cardoso <115185989+cardosojse@users.noreply.github.com>
2023-09-18 16:07:37 -0300

    modified

--- app.js
+++ app.js
@@ -1,37 +1,16 @@
-let categoryImages = [
-    {
-        option: card,
-        img: images/options-img/card.png,
-    },
-    {
-        option: eletricity,
-        img: images/options-img/eletricity.png,
-    },
-    {
-        option: food,
-        img: images/options-img/food.png,
-    },
-    {
-        option: house,
-        img: images/options-img/house.png,
-    },
-    {
-        option: internet,
-        img: images/options-img/internet.png,
-    },
-    {
-        option: Pet,
-        img: images/options-img/pet.png,
-    },
-    {
-        option: Trip,
-        img: images/options-img/trip.png,
-    },
-    {
-        option: others,
-        img: images/options-img/what.png, 
-    }
-];
+const images = {
+    Card: 'images/options-img/card.png',
+    Eletricity: 'images/options-img/eletricity.png',
+    Food: 'images/options-img/food.png',
+    House: 'images/options-img/house.png',
+    Internet: 'images/options-img/internet.png',
+    Pet: 'images/options-img/pet.png',
+    Pharmacy: 'images/options-img/pharmacy.png',
+    Shopping: 'images/options-img/shopping.png',
+    Travel: 'images/options-img/trip.png',
+    Others: 'images/options-img/what.png', 
+}
+
 const toggleBtn = document.querySelector(".sidebar-toggle"); 
 const closeBtn = document.querySelector(".close-btn");
 const sidebar = document.querySelector(".sidebar");
@@ -41,6 +20,14 @@ const moneybox = document.querySelector(".moneybox");
 const closeMoneybox = document.querySelector(".close-moneybox");
 const moneyInput = document.querySelector("#moneybox-input");
 const moneySubmit = document.querySelector("#moneybox-submit");
+const optionImg = document.querySelector(".option-img");
+const transactionCategory = document.querySelector("#transaction-category");
+
+// Change default image of select element
+transactionCategory.addEventListener("click", ()=>{
+    let category = categorySelect.value;
+    optionImg.src = images[category];
+})
 
 // display sidebar
 toggleBtn.addEventListener("click", () => {
@@ -68,28 +55,17 @@ closeMoneybox.addEventListener("click", () => {
 
 // add money to wallet
 moneySubmit.addEventListener("click", () => {
-    if (moneyInput.value === ''){
-        alert('Field is empty!');
-    }else{
-        let num = moneyInput.value;
-        // wallet.innerText = num;
-        wallet.textContent = new Intl.NumberFormat('pt-BR', { style: 'currency', 
-            currency: 'BRL' }).format(num);
+    let transactionValue = Number(moneyInput.value);
+    if (isNaN(transactionValue) || transactionValue <= 0){
+        alert('Enter a valid amount!');
+        return
     }
+    walletAmount(transactionValue);
     moneyInput.value = '';
     moneybox.classList.remove("show-money-bar");
     saveData();
-    console.log(wallet.innerText);
-})
-let saveData = () => {
-    localStorage.setItem("data", wallet.innerText);
-}
-let getData = () => {
-    wallet.innerText = localStorage.getItem("data");
-}
-getData();
+});
 
-//
 let totalAmount = 0;
 const amountInput = document.querySelector("#transaction-value");
 const categorySelect = document.querySelector("#transaction-category");
@@ -100,14 +76,12 @@ const totalSpent = document.querySelector(".total-spent");
 const todayDate = document.querySelector(".current-date");
 const expenseList = document.querySelector(".expense-list");
 
+// add transaction
 addBtn.addEventListener("click", () => {
-
-    // let walletAmount = parseFloat(wallet.innerText);
     let amount = Number(amountInput.value);
     let category = categorySelect.value;
     // let date = dateInput.value;
     // let notes = noteInput.value;
-
     if (category === ''){
         alert('Select a category!');
         return;
@@ -120,27 +94,48 @@ addBtn.addEventListener("click", () => {
     //     alert('Select a date!');
     //     return;
     // }
+    totalAmount += amount;
 
+    totalSpent.innerText = new Intl.NumberFormat('pt-BR', 
+        { style: 'currency', currency: 'BRL' }).format(totalAmount);
+
+    newExpense(images[category], category, amount);
+    walletAmount(amount);
     amountInput.value = '';
     sidebar.classList.remove("show-sidebar");
-
-    totalAmount += amount;
-    
-    // walletAmount -= amount;
-    // wallet.innerText = walletAmount;
-    
-    newExpense(category, amount);
-
-    totalSpent.textContent = new Intl.NumberFormat('pt-BR', 
-        { style: 'currency', currency: 'BRL' }).format(totalAmount);
-    // wallet.innerText = new Intl.NumberFormat('pt-BR', { style: 'currency', 
-    //     currency: 'BRL' }).format(walletAmount);
+    saveData();
 });
 
-let newExpense = (ctg, amt) => {
+let newExpense = (img, ctg, amt) => {
     let li = document.createElement("li");
-    li.innerHTML += `<img src="">
+    li.innerHTML += `<img src="${img}">
                     <span class="spent-on">${ctg}</span>
                     <span class="money-spent">${amt}</span>`;
     expenseList.appendChild(li);
 }
+
+let walletAmount = (num) => {
+    let walletAmount = parseFloat(wallet.innerText);
+    if (moneyInput.value === ''){
+        walletAmount -= num;
+    }
+    else if(amountInput.value === ''){
+        walletAmount += num;
+    }
+    wallet.innerText = Math.round(walletAmount);
+}
+
+let saveData = () => {
+    localStorage.setItem("wallet-data", wallet.innerText);
+    localStorage.setItem("expense-list", expenseList.innerHTML);
+    localStorage.setItem("total-spent", totalSpent.innerText);
+
+}
+let getData = () => {
+    wallet.innerText = localStorage.getItem("wallet-data");
+    expenseList.innerHTML = localStorage.getItem("expense-list");
+    totalSpent.textContent = localStorage.getItem("total-spent");
+    // localStorage.removeItem("total-spent");
+}
+
+getData();
